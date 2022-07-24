2575f7e8183c8fdfeafc10dcef2c35049cdc9244
lsh <fantasyz7@naver.com>
2022-07-24 16:56:25 +0900

    mustache use

---
+++ sample2/src/main/java/com/example/sample2/controller/FirstController.java
@@ -0,0 +1,17 @@
+package com.example.sample2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstController {
+  /**
+   * @return
+   */
+  @GetMapping("/")
+  public String greet(Model model) {
+    model.addAttribute("username", "metaxy");
+    return "greetings";// mustache file name
+  }
+}
