cc03a4dce2e805720419e196397cea8f5587b606
lsh <fantasyz7@naver.com>
2022-07-25 06:39:00 +0900

    data view

--- sample2/src/main/java/com/example/sample2/controller/ArticleController.java
+++ sample2/src/main/java/com/example/sample2/controller/ArticleController.java
@@ -4,9 +4,12 @@ import com.example.sample2.dto.ArticleForm;
 import com.example.sample2.entity.Article;
 import com.example.sample2.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -31,5 +34,14 @@ public class ArticleController {
     // System.out.println(saved.toString());
     log.info(saved.toString());
     return "";
+  }
+
+  @GetMapping("/articles/{id}")
+  public String show(@PathVariable Long id, Model model) {
+    log.info("id = " + id);
+    Optional<Article> articleEntity = articleRepository.findById(id);
+    // Article articleEntity = articleRepository.findById(id).orElse(null);
+    model.addAttribute("article", articleEntity);
+    return "articles/show";
   }
 }
