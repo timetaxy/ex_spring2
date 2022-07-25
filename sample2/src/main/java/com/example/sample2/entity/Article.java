d8a7ffbd611c6fe3e5063a743656724c5c42ea11
lsh <fantasyz7@naver.com>
2022-07-25 23:40:53 +0900

    sql debug log, id generate

--- sample2/src/main/java/com/example/sample2/entity/Article.java
+++ sample2/src/main/java/com/example/sample2/entity/Article.java
@@ -3,6 +3,7 @@ package com.example.sample2.entity;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -16,7 +17,7 @@ import lombok.ToString;
 @Getter
 public class Article {
   @Id
-  @GeneratedValue
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   @Column
   private String title;
